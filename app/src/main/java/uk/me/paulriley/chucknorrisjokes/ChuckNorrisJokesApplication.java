00a4e67c16902355b331cef2649dfcf9d97371e3
Paul Riley <pauljriley@gmail.com>
2016-12-05 16:20:30 +0000

    add crashlytics & answers

--- app/src/main/java/uk/me/paulriley/chucknorrisjokes/ChuckNorrisJokesApplication.java
+++ app/src/main/java/uk/me/paulriley/chucknorrisjokes/ChuckNorrisJokesApplication.java
@@ -3,7 +3,9 @@ package uk.me.paulriley.chucknorrisjokes;
 import android.app.Application;
 import android.content.Context;
 
+import com.crashlytics.android.Crashlytics;
 import dagger.ObjectGraph;
+import io.fabric.sdk.android.Fabric;
 import uk.me.paulriley.chucknorrisjokes.injection.Injector;
 import uk.me.paulriley.chucknorrisjokes.injection.modules.ApplicationModule;
 
@@ -14,6 +16,7 @@ public class ChuckNorrisJokesApplication extends Application implements Injector
     @Override
     public void onCreate() {
         super.onCreate();
+        Fabric.with(this, new Crashlytics());
 
         ApplicationModule applicationModule = new ApplicationModule();
         objectGraph = ObjectGraph.create(applicationModule);
